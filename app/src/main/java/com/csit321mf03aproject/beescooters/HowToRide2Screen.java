a9621fc6563af8453957782fa0961b166a3c93cc
Aaron <nephorix@gmail.com>
2018-08-09 00:56:57 +1000

    Converted some designs from XD to Android Studio

---
+++ app/src/main/java/com/csit321mf03aproject/beescooters/HowToRide2Screen.java
@@ -0,0 +1,12 @@
+package com.csit321mf03aproject.beescooters;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class HowToRide2Screen extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.howtoride2_screen);
+    }
+}
---
+++ app/src/main/java/com/csit321mf03aproject/beescooters/ParkingScreen.java
@@ -0,0 +1,12 @@
+package com.csit321mf03aproject.beescooters;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ParkingScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.parking_screen);
+    }
+}
